d50394c1999cfe382d46cde904a12f59f5f4ce6f
Yuto Nakajima <yutonkjima1108@gmail.com>
2023-11-07 19:18:57 +0900

    電動車いすの状態（停止中か走行中か）をノートPC1に送信するノードを追加

--- communication/LiDAR.py
+++ communication/LiDAR.py
@@ -4,10 +4,12 @@ import csv
 import math
 import socket
 import pickle
+import struct
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py import point_cloud2 #Point Cloud2を読み取るためのモジュール
 from ros2_whill.msg import BoolMultiArray
+from std_msgs.msg import String
 
 class LiDAR_Subscriber(Node):
 
@@ -62,23 +64,54 @@ class Send_Obstacle_Info(Node):
     def  __init__(self):
         super().__init__('send_obstacle_info')
         self.sub = self.create_subscription(BoolMultiArray, 'obstacle_info', self.obstacle_info_callback, 10)
+        self.sub_socket = self.create_subscription(String, 'socket_topic', self.state_info_callback, 10)
         
-        # サーバーソケットを作成
-        server_address = ('192.168.1.102', 50000)
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind(server_address)
-        self.server_socket.listen(1)
+        # 障害物情報を送信するサーバーソケットを作成
+        server_address_obstacle = ('192.168.1.102', 50000)
+        self.server_socket_o = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket_o.bind(server_address_obstacle)
+        self.server_socket_o.listen(1)
+        
+        # 電動車いすの状態を送信するサーバーソケットを作成
+        server_address_state = ('192.168.1.102', 50010)
+        self.server_socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket_s.bind(server_address_state)
+        self.server_socket_s.listen(1)
+        
+        #self.state = False
         
     '''障害物情報を購読してノートPC1に送信''' 
     def obstacle_info_callback(self, obstacle_msg):
-        
-        client_socket, client_address = self.server_socket.accept()   # クライアントからの接続を待機
-        print("クライアントが接続しました:", client_address)
-        
-        data = pickle.dumps(obstacle_msg.data)    # ブール値の配列をバイト列に変換
-        client_socket.send(data)
-        
-        client_socket.close()
+        try:
+            self.client_socket_o, self.client_address_o = self.server_socket_o.accept()   # クライアントからの接続を待機
+            #print("クライアントが接続しました:", self.client_address_o)     
+            data_o = pickle.dumps(obstacle_msg.data)    # ブール値の配列をバイト列に変換
+            self.client_socket_o.send(data_o) 
+            self.client_socket_o.close()
+        except:
+            print("クライアントが接続を切断しました")
+            self.client_socket_o.close()
+            self.server_socket_o.close()
+               
+    '''socket_topicの文字を購読して電動車いすの状態をノートPC1に送信'''            
+    def state_info_callback(self, state_msg):
+        if state_msg.data == 'q':
+            self.client_socket_o.close()
+            self.server_socket_o.close()
+            self.client_socket_s.close()
+            self.server_socket_s.close()
+        else:
+            self.client_socket_s, self.client_address_s = self.server_socket_s.accept()   # クライアントからの接続を待機
+            print("クライアントが接続しました:", self.client_address_s) 
+            print("購読文字：", state_msg.data)
+            if state_msg.data == 's':
+                bool_byte = struct.pack('?', False)
+                self.client_socket_s.send(bool_byte)
+                self.client_socket_s.close()
+            else:
+                bool_byte = struct.pack('?', True)
+                self.client_socket_s.send(bool_byte)
+                self.client_socket_s.close()
     
 def main():
     rclpy.init()
@@ -91,10 +124,11 @@ def main():
         executor.spin()
     except KeyboardInterrupt:
         pass
-    executor.shutdown()
-    lidar_sub_node.destroy_node()
-    socket_obstacle_node.destroy_node()
-    rclpy.shutdown()
+    finally:
+        executor.shutdown()
+        lidar_sub_node.destroy_node()
+        socket_obstacle_node.destroy_node()
+        rclpy.shutdown()
     
 if __name__ == '__main__':
     main()
