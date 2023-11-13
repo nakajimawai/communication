c795d87c31d6338313cf9238f83c11232b3e4639
Yuto Nakajima <yutonkjima1108@gmail.com>
2023-11-13 17:28:41 +0900

    電動車いすの状態を送信するプロセスをノード化

--- communication/LiDAR.py
+++ communication/LiDAR.py
@@ -10,6 +10,7 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py import point_cloud2 #Point Cloud2を読み取るためのモジュール
 from ros2_whill.msg import BoolMultiArray
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 class LiDAR_Subscriber(Node):
 
@@ -48,10 +49,13 @@ class LiDAR_Subscriber(Node):
                         self.obstacle_info.data[3] = True
                 
                 elif (45 < direction <135):   #左方向に検出した場合
-                        self.obstacle_info.data[2] = True 
+                        #if math.sqrt((x**2)+(y**2)) <= 0.5:
+                            self.obstacle_info.data[2] = True 
 
                 elif (-135 < direction <-45):   #右方向に検出した場合
-                        self.obstacle_info.data[1] = True 
+                    #if x == 0:
+                        #if math.sqrt((x**2)+(y**2)) <= 0.5:
+                            self.obstacle_info.data[1] = True 
 
             #print(self.obstacle_info)
             self.pub.publish(self.obstacle_info)
@@ -59,28 +63,20 @@ class LiDAR_Subscriber(Node):
         else:
             #self.get_logger().info("No obstacles detected.")
             self.pub.publish(self.obstacle_info)
-            
+    
+'''障害物情報を購読してノートPC1に送信するノード'''         
 class Send_Obstacle_Info(Node):
     def  __init__(self):
         super().__init__('send_obstacle_info')
         self.sub = self.create_subscription(BoolMultiArray, 'obstacle_info', self.obstacle_info_callback, 10)
-        self.sub_socket = self.create_subscription(String, 'socket_topic', self.state_info_callback, 10)
-        
+        #self.sub_socket = self.create_subscription(Twist, 'cmd_vel', self.state_info_callback, 10)
+        #self.sub_socket = self.create_subscription(String, 'socket_topic', self.state_info_callback, 10)
         # 障害物情報を送信するサーバーソケットを作成
         server_address_obstacle = ('192.168.1.102', 50000)
         self.server_socket_o = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket_o.bind(server_address_obstacle)
         self.server_socket_o.listen(1)
-        
-        # 電動車いすの状態を送信するサーバーソケットを作成
-        server_address_state = ('192.168.1.102', 50010)
-        self.server_socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket_s.bind(server_address_state)
-        self.server_socket_s.listen(1)
-        
-        #self.state = False
-        
-    '''障害物情報を購読してノートPC1に送信''' 
+
     def obstacle_info_callback(self, obstacle_msg):
         try:
             self.client_socket_o, self.client_address_o = self.server_socket_o.accept()   # クライアントからの接続を待機
@@ -88,38 +84,56 @@ class Send_Obstacle_Info(Node):
             data_o = pickle.dumps(obstacle_msg.data)    # ブール値の配列をバイト列に変換
             self.client_socket_o.send(data_o) 
             self.client_socket_o.close()
-        except:
+        except :
             print("クライアントが接続を切断しました")
             self.client_socket_o.close()
             self.server_socket_o.close()
                
-    '''socket_topicの文字を購読して電動車いすの状態をノートPC1に送信'''            
+
+'''socket_topicの文字を購読して電動車いすの状態をノートPC1に送信するノード'''
+class Send_State(Node):
+    def  __init__(self):
+        super().__init__('send_state_info')
+        self.sub_socket = self.create_subscription(String, 'socket_topic', self.state_info_callback, 10)
+        
+        server_address_state = ('192.168.1.102', 50010)
+        self.server_socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket_s.bind(server_address_state)
+        self.server_socket_s.listen(1)
+        
+        self.state = False
+
     def state_info_callback(self, state_msg):
+        print("状態を購読", state_msg)
+        
         if state_msg.data == 'q':
-            self.client_socket_o.close()
-            self.server_socket_o.close()
             self.client_socket_s.close()
             self.server_socket_s.close()
-        else:
+            print("切断")
+        elif state_msg.data != 'q':
             self.client_socket_s, self.client_address_s = self.server_socket_s.accept()   # クライアントからの接続を待機
-            print("クライアントが接続しました:", self.client_address_s) 
-            print("購読文字：", state_msg.data)
+            print("状態受信用クライアントが接続しました:", self.client_address_s) 
             if state_msg.data == 's':
                 bool_byte = struct.pack('?', False)
                 self.client_socket_s.send(bool_byte)
+                print('Falseを送信')
                 self.client_socket_s.close()
             else:
                 bool_byte = struct.pack('?', True)
                 self.client_socket_s.send(bool_byte)
+                print('Trueを送信')
                 self.client_socket_s.close()
-    
+        
+
 def main():
     rclpy.init()
     lidar_sub_node = LiDAR_Subscriber()
     socket_obstacle_node = Send_Obstacle_Info()
+    socket_state_node = Send_State()
     executor = rclpy.executors.MultiThreadedExecutor()
     executor.add_node(lidar_sub_node)
     executor.add_node(socket_obstacle_node)
+    executor.add_node(socket_state_node)
     try:
         executor.spin()
     except KeyboardInterrupt:
@@ -128,6 +142,7 @@ def main():
         executor.shutdown()
         lidar_sub_node.destroy_node()
         socket_obstacle_node.destroy_node()
+        socket_state_node.destroy_node()
         rclpy.shutdown()
     
 if __name__ == '__main__':
